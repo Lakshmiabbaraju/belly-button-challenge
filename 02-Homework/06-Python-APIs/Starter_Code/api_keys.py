f21e077c7e0a3b991a6e5e6fb0af27c35432c19e
Lakshmiabbaraju <lakshmi.abbaraju@usaa.com>
2024-10-19 15:59:54 -0500

    Add WeatherPy notebook and update .gitignore

--- 02-Homework/06-Python-APIs/Starter_Code/api_keys.py
+++ 02-Homework/06-Python-APIs/Starter_Code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "d6c45e21ebc9543b0b15920a43fb5e91"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "6310a7235470498fad90aa960451ee86"
